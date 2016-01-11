f68838e96675881d857fb4dff79c308ba98ac418
Burlak Ilia <i.l.burlak@gmail.com>
2016-01-11 19:02:15 +0300

    Add simple unit test

---
+++ public_html/js/index.js
@@ -0,0 +1,31 @@
+function showScoreboardScreen() {
+	// TODO
+}
+function hideScoreboardScreen() {
+	// TODO
+}
+
+function showGameScreen() {
+	// TODO
+}
+function hideGameScreen() {
+	// TODO
+}
+
+function showLoginScreen() {
+	// TODO
+}
+function hideLoginScreen() {
+	// TODO
+}
+
+function showMainScreen() {
+	var html = window.mainTmpl()
+	$page.html(html);
+	// TODO
+}
+
+function hideMainScreen() {
+	// TODO
+}
+
