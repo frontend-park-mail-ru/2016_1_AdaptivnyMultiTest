9d2835649481342b790309a28eb2033814891d51
Burlak Ilia <i.l.burlak@gmail.com>
2016-02-08 18:18:11 +0300

    fdsafasd

--- server.js
+++ server.js
@@ -2,21 +2,19 @@ var express = require("express"),
     errorHandler = require('errorhandler'),
     app = express();
 
-
 var HOSTNAME = 'localhost',
     PORT = 8080,
     PUBLIC_DIR = __dirname + '/public_html';
-
-
-app.use('/', express.static(PUBLIC_DIR));
 
 app.use(function (req) {
 	// Здесь нужно написать журналирование в формате
 	// [время] [method] uri (* номер запроса по счету)
 });
 
-app.use(errorHandler());
+app
+	.use('/', express.static(PUBLIC_DIR))
+	.use(errorHandler());
 
-app.listen(PORT, function (argument) {
+app.listen(PORT, function () {
 	console.log("Simple static server showing %s listening at http://%s:%s", PUBLIC_DIR, HOSTNAME, PORT);
 });
